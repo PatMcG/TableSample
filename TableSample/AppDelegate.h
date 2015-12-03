b0eebc1ecd54dda332f26a2a2ba0a3429d5c0f84
Patrick <patrick.mcgarity@uconn.edu>
2015-12-03 15:04:50 -0500

    Initial Commit

---
+++ TableSample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TableSample
+//
+//  Created by Patrick McGarity on 12/3/15.
+//  Copyright © 2015 Patrick McGarity. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TableSample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TableSample
+//
+//  Created by Patrick McGarity on 12/3/15.
+//  Copyright © 2015 Patrick McGarity. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
